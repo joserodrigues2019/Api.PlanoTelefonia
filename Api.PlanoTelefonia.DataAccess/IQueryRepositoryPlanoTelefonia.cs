855db6e2c51509821739b8e2277bd01a73c4e172
joserodrigues2019 <rodrigues.santos@gmail.com>
2020-10-12 15:58:11 -0400

    Implementação do repositorio para listar os registros.

---
+++ Api.PlanoTelefonia.DataAccess/IQueryRepositoryPlanoTelefonia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Api.PlanoTelefonia.DataAccess
+{
+    public interface IQueryRepositoryPlanoTelefonia<T> where T : BaseEntity
+    {
+        List<U> Listar<U>(Expression<Func<T, bool>> predicate);
+        List<T> Listar(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+                string includeProperties = "");
+    }
+}
---
+++ Api.PlanoTelefonia.DataAccess/QueryRepositoryPlanoTelefonia.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Api.PlanoTelefonia.DataAccess
+{
+    public class QueryRepositoryPlanoTelefonia<T> : IQueryRepositoryPlanoTelefonia<T> where T : BaseEntity
+	{
+		private readonly IDatabaseContextPlanoTelefonia _dbContext;
+
+        public QueryRepositoryPlanoTelefonia(IDatabaseContextPlanoTelefonia dbContext)
+        {
+			_dbContext = dbContext;
+
+		}
+
+		public List<U> Listar<U>(Expression<Func<T, bool>> predicate)
+		{
+			return null;
+			//return AutoMapper.Mapper.Map<List<U>>(this.Listar(predicate));
+		}
+
+		/// <summary>
+		/// https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application
+		/// </summary>
+		/// <param name="filter"></param>
+		/// <param name="orderBy"></param>
+		/// <param name="includeProperties"></param>
+		/// <returns></returns>
+		public List<T> Listar(Expression<Func<T, bool>> filter = null,
+			Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+			string includeProperties = "")
+		{
+			IQueryable<T> query = _dbContext.Set<T>();
+
+			if (filter != null)
+			{
+				query = query.Where(filter);
+			}
+
+			foreach (var includeProperty in includeProperties.Split
+				(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+			{
+				query = query.Include(includeProperty);
+			}
+
+			if (orderBy != null)
+			{
+				return orderBy(query)
+					.AsNoTracking()
+					.ToList();
+			}
+			else
+			{
+				return query
+					.AsNoTracking()
+					.ToList();
+			}
+		}
+
+	}
+}
