005201b623a744af12268fdb3fa412dbdc58cb5e
joserodrigues2019 <rodrigues.santos@gmail.com>
2020-10-12 14:19:08 -0400

    Adicionar arquivos de projeto.

---
+++ Api.PlanoTelefonia.DataAccess/Entities/PlanoTelefoniaEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.PlanoTelefonia.DataAccess.Entities
+{
+    [Table("PLANO_TELEFONIA")]
+    public class PlanoTelefoniaEntity
+    {
+        public virtual PlanoTipoEntity PlanoTipo { get; set; }
+
+        public PlanoTelefoniaEntity()
+        {
+            PlanoTipo = new PlanoTipoEntity();
+        }
+
+        [Key]
+        [Column("PLANO_ID")]
+        public int IdPlano { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Column("PLANO_CODIGO")]
+        public string Codigo { get; set; }
+
+        [Required]
+        [Column("PLANO_MINUTOS")]
+        public int Minutos { get; set; }
+
+        [StringLength(50)]
+        [Column("PLANO_FRANQUIA_INTERNET")]
+        public string FranquiaInternet { get; set; }
+
+        [Column("PLANO_VALOR")]
+        public decimal Valor { get; set; }
+
+        [Required]
+        [Column("PLANO_ID_TIPO")]
+        [ForeignKey(nameof(PlanoTipo))]
+        public int IdPlanoTipo { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [Column("PLANO_DATA_CADASTRO")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ Api.PlanoTelefonia.DataAccess/Entities/PlanoTipoEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.PlanoTelefonia.DataAccess.Entities
+{
+    [Table("PLANO_TIPO")]
+    public class PlanoTipoEntity
+    {
+        [Key]
+        [Column("PLANO_ID_TIPO")]
+        public int IdPlanoTipo { get; set; }
+
+        [Required]
+        [Column("PLANO_DESCRICAO_TIPO")]
+        [StringLength(50)]
+        public string DescricaoTipo { get; set; }
+    }
+}
